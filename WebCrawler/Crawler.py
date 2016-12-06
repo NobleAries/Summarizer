63cdb8f900cc52fb132a0a3e9915c0e547813a01
Joanna Byra <byra.joann@gmail.com>
2016-12-07 00:43:28 +0100

    Crawler prototype

---
+++ WebCrawler/Crawler.py
@@ -0,0 +1,11 @@
+class Crawler(object):
+	def __init__(self, url):
+		self.url = url
+		
+	def get_content(self):
+		f = open(self.url, "r")
+		content = f.read()
+		f.close()
+		return content
+		
+		
