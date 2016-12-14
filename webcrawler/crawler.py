efacd99c8d3bacf1bd5717800568479094530989
NobleAries <michalasakura@gmail.com>
2016-12-14 17:42:07 +0100

    Formatted lines in project

--- webcrawler/crawler.py
+++ webcrawler/crawler.py
@@ -1,9 +1,9 @@
 class Crawler(object):
-	def __init__(self, url):
-		self.url = url
-		
-	def get_content(self):
-		f = open(self.url, "r")
-		content = f.read()
-		f.close()
-		return content
+    def __init__(self, url):
+        self.url = url
+
+    def get_content(self):
+        f = open(self.url, "r")
+        content = f.read()
+        f.close()
+        return content
