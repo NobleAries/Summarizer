08376265e03dd3e65f5a89282b41a0f3afa4a29b
Piotr Przybyło <pitu5599@gmail.com>
2016-12-14 17:15:21 +0100

    Added new argument: name of file to store result of summarization

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Summarize text in file')
-    parser.add_argument('file', help='name of file containing text to summarize')
-    parser.add_argument('sentences', type=int, help='number of sentences in summary')
+    parser.add_argument('infile', help='name of file containing text to summarize')
+    parser.add_argument('--sentences', type=int, help='number of sentences in summary', default=3)
+    parser.add_argument('--outfile', help='name of file to store result of summarization', default=None)
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
-    crawler = Crawler(args.file)
-    summary = summarize(text=crawler.get_content(), number_of_sentences=args.sentences)
+    crawler = Crawler(args.infile)
+    summary = summarize(text=crawler.get_content(), number_of_sentences=args.sentences, outfile=args.outfile)
     print(summary)
--- summarizer/summarize.py
+++ summarizer/summarize.py
@@ -1,6 +1,10 @@
 import nltk
 
 
-def summarize(text, number_of_sentences):
+def summarize(text, number_of_sentences=3, outfile=None):
     sentences = nltk.sent_tokenize(text)
-    return '\n'.join(sentences[:number_of_sentences])
+    summarized_text = '\n'.join(sentences[:number_of_sentences])
+    if outfile is not None:
+        with open(outfile, 'w') as file:
+            file.write(summarized_text)
+    return summarized_text
