f680a26caac75eeb2e3226845d2598e889d576a7
lcynarski <l.cynarski@gmail.com>
2017-01-06 12:54:56 +0100

    Added Article class with title and text. get_content now returns list of Article objects. Added necessary requirements.

--- webcrawler/crawler.py
+++ webcrawler/crawler.py
@@ -3,6 +3,12 @@ import requests
 import re
 
 
+class Article:
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+
+
 class Crawler(object):
     def __init__(self, url):
         self.url = url
@@ -23,21 +29,27 @@ class Crawler(object):
             if pattern.match(link.get('href')):
                 art_r = requests.get(link.get('href'))
                 article_soup = BeautifulSoup(art_r.text, "lxml")
+                article_title = article_soup.find('title')
                 article_text = article_soup.find('div', {'property': 'articleBody'})
-                if article_text is not None:
+                if article_text:
+                    txt = []
                     for at in article_text.find_all('p'):
-                        if at is not None:
-                            print(at.text)
-                            articles.append(at.text)
+                        if at:
+                            txt.append(at.text)
+                            article = Article(article_title.text, txt)
+                            articles.append(article)
             else:
                 absolute_link = self.url + link.get('href')
                 art_r = requests.get(absolute_link)
                 article_soup = BeautifulSoup(art_r.text, "lxml")
+                article_title = article_soup.find('title')
                 article_text = article_soup.find('div', {'property': 'articleBody'})
-                if article_text is not None:
+                if article_text:
+                    txt = []
                     for at in article_text.find_all('p'):
-                        if at is not None:
-                            print(at.text)
-                            articles.append(at.text)
+                        if at:
+                            txt.append(at.text)
+                    article = Article(article_title.text, txt)
+                    articles.append(article)
 
         return articles
