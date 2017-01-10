4b317731b966d0ddffa07a6903ea8c14f8b6c867
Joanna Byra <byra.joann@gmail.com>
2017-01-10 21:43:52 +0100

    Added function for processing articles

--- webcrawler/crawler.py
+++ webcrawler/crawler.py
@@ -13,37 +13,39 @@ class Article:
 class Crawler(object):
     def __init__(self, url):
         self.url = url
-        self.found_links = []
+        self._found_links = []
         self.articles = []
-        self.links_lock = threading.Lock()
-        self.articles_lock = threading.Lock()
+        self._links_lock = threading.Lock()
+        self._articles_lock = threading.Lock()
 
-    def process_links(self):
+    def _process_articles(self, link):
+        art_r = requests.get(link)
+        article_soup = BeautifulSoup(art_r.text, "lxml")
+        article_title = article_soup.find('title')
+        article_text = article_soup.find('div', {'property': 'articleBody'})
+        if article_text:
+            txt = []
+            for at in article_text.find_all('p'):
+                if at:
+                    txt.append(at.text)
+            return Article(article_title.text, " ".join(txt))
+
+    def _process_links(self):
 
         pattern = re.compile("^https?://")
-        self.links_lock.acquire()
-        while self.found_links:
-            link = self.found_links.pop()
-            self.links_lock.release()
-
+        self._links_lock.acquire()
+        while self._found_links:
+            link = self._found_links.pop()
+            self._links_lock.release()
             absolute_link = link.get('href') if pattern.match(link.get('href')) else self.url + link.get('href')
-
-            art_r = requests.get(absolute_link)
-            article_soup = BeautifulSoup(art_r.text, "lxml")
-            article_title = article_soup.find('title')
-            article_text = article_soup.find('div', {'property': 'articleBody'})
-            if article_text:
-                txt = []
-                for at in article_text.find_all('p'):
-                    if at:
-                        txt.append(at.text)
-                article = Article(article_title.text, " ".join(txt))
-                self.articles_lock.acquire()
+            article = self._process_articles(absolute_link)
+            if article:
+                self._articles_lock.acquire()
                 self.articles.append(article)
-                self.articles_lock.release()
+                self._articles_lock.release()
 
-            self.links_lock.acquire()
-        self.links_lock.release()
+            self._links_lock.acquire()
+        self._links_lock.release()
 
 
 
@@ -52,12 +54,10 @@ class Crawler(object):
 
         r = requests.get(self.url)
         soup = BeautifulSoup(r.text, "lxml")
-
-        self.found_links = soup.find_all('a', {'class': 'media__link'}, href=True)
-
+        self._found_links = soup.find_all('a', {'class': 'media__link'}, href=True)
         threads = []
         for i in range(0, 6):
-            threads.append(threading.Thread(target=self.process_links))
+            threads.append(threading.Thread(target=self._process_links))
             threads[i].start()
 
         for t in threads:
